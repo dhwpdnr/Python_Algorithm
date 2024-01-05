d024ccddbe5e4689f320567cc122241669a665e2
dhwpdnr <ink1237@gmail.com>
2024-01-05 12:57:55 +0900

    [feat] merge list

--- sort_test.py
+++ sort_test.py
@@ -26,3 +26,25 @@ def quick_sort(A, hi, lo):
         pivot = partition(lo, hi)
         quick_sort(A, lo, pivot - 1)
         quick_sort(A, pivot + 1, hi)
+
+
+# 구간 병합
+# 겹치는 구간을 병합
+# 입력: [[1, 3], [2, 6], [8, 10], [15, 18]]
+# 출력: [[1, 6], [8, 10], [15, 18]]
+
+# 정렬하여 병합
+# 시간 복잡도 : O(n log n)
+def merge(intervals):
+    merged = []
+    for i in sorted(intervals, key=lambda x: x[0]):
+        if merged and i[0] <= merged[-1][1]:
+            merged[-1][1] = max(merged[-1][1], i[1])
+        else:
+            merged += i,
+    return merged
+
+
+q = merge([[1, 3], [2, 6], [8, 10], [15, 18]])
+assert q == [[1, 6], [8, 10], [15, 18]]
+print(q)
