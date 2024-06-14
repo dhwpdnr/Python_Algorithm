1ba2d0a6d8ee67d0dbf0cc7f83d4298ef3951773
dhwpdnr <ink1237@gmail.com>
2024-06-14 11:07:23 +0900

    [feat] BFS DFS

---
+++ dfs.py
@@ -0,0 +1,23 @@
+def dfs(g, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in g[v]:
+        if not visited[i]:
+            dfs(g, i, visited)
+
+
+g = [
+    [],
+    [2, 3, 8],
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1, 7],
+]
+
+visited = [False] * 9
+
+dfs(g, 1, visited)
